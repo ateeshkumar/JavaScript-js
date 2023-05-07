b31b14bbb9c2372aa3e1b31d9ee2f8d21e5a45ec
ateeshkumar <ateesh2002@gmail.com>
2023-05-07 22:01:38 +0530

    Done

--- 7-Event Handiling/script.js
+++ 7-Event Handiling/script.js
@@ -1,13 +1,13 @@
 //  1-EventListener
 
-function onClear(){
-    alert('Clear All element');
-}
-const clearBtn = document.querySelector('#btn-1');
-clearBtn.onClick = function(){
-    alert('Clear Item');
+// function onClear(){
+//     alert('Clear All element');
+// }
+// const clearBtn = document.querySelector('#btn-1');
+// clearBtn.onClick = function(){
+//     alert('Clear Item');
 
-}
+// }
 
 
 // clearBtn.addEventListener('click',function(){
@@ -19,12 +19,12 @@ clearBtn.onClick = function(){
 // clearBtn.addEventListener('click',()=>{
 //     alert("All Clear");
 // });
-clearBtn.addEventListener('click',onClear);
+// clearBtn.addEventListener('click',onClear);
 // setTimeout(()=> clearBtn.removeEventListener('click',onClear()),5000);
 // setTimeout(()=>clearBtn.click(),5000);
 
 //   2-Mouse Events
-const logo = document.querySelector('.img');
+// const logo = document.querySelector('.img');
 // const onClick = ()=>{
 //     // document.body.style.backgroundColor='purple';
 //    if (document.body.style.backgroundColor !=='purple') {
@@ -84,18 +84,48 @@ const logo = document.querySelector('.img');
 
 // 4 - KeyBoard Event
 
-const input = document.querySelector('input');
-const onKeyPress = e=>{
-    const h1 = document.querySelector('h1');
-    h1.append(e.key);
-}
-const onKeyDown = e=>{
-    console.log('Press Down');
-}
-const onKeyUp = e=>{
-    console.log('Press Up');
-}
+// const input = document.querySelector('input');
+// const onKeyPress = e=>{
+//     const h1 = document.querySelector('h1');
+//     h1.append(e.key);
+// }
+// const onKeyDown = e=>{
+//     console.log('Press Down');
+// }
+// const onKeyUp = e=>{
+//     console.log('Press Up');
+// }
 
-input.addEventListener('keypress',onKeyPress);
-input.addEventListener('keydown',onKeyDown);
-input.addEventListener('keyup',onKeyUp);
+// input.addEventListener('keypress',onKeyPress);
+// input.addEventListener('keydown',onKeyDown);
+// input.addEventListener('keyup',onKeyUp);
+
+//challenges
+
+// const con = document.querySelector('h2');
+// const col1 = document.querySelector('.col')
+
+
+// window.addEventListener('keydown',()=>{
+//     const insert = document.getElementsByClassName('col-1');
+
+// });
+
+// con.addEventListener('keypress',onKeyPress);
+
+// input Event
+const itemInput= document.querySelector('.form-input');
+const priorityInput = document.querySelector('#priority-input');
+const checkbox = document.querySelector('#checkbox');
+const head = document.querySelector('h2');
+
+
+itemInput.addEventListener('input',(e)=>{
+    // console.log(e.target.value);
+    head.textContent = e.target.key;
+});
+priorityInput.addEventListener('input',(e)=>{
+    head.textContent = e.target.value;
+});
+
+
